2cdcd2aec733339655816b190950e74b0926fada
AjaykrShrma <153522246+AjaykrShrma@users.noreply.github.com>
2023-12-11 20:25:06 +0530

    Create Anagram.java

---
+++ Anagram.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagrams
+{
+    void anag()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter two Strings ");
+        String str1=sc.next();
+        String str2=sc.next();
+        System.out.println("String1= "+str1+"\n"+"String2= "+str2);
+       boolean status=true;
+       if (str1.length()!=str2.length())
+       status=false;
+       else
+       {
+           char[] Arstr1= str1.toLowerCase().toCharArray();
+           char[] Arstr2=str2.toLowerCase().toCharArray();
+           Arrays.sort(Arstr1);
+           Arrays.sort(Arstr2);
+           status=Arrays.equals(Arstr1,Arstr2);
+       }
+       if (status){
+           System.out.println("Anagram");
+       }
+       else System.out.println("Not Anagram");
+    }
+    public static void main(String[]ak)
+    {
+        Anagrams an=new Anagrams();
+        an.anag();
+    }
+}
